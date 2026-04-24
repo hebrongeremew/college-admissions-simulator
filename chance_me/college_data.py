3de6eb7be1605b3db7ed9d0351ace7660c74b943
jknoble <jahrel03@gmail.com>
2026-04-24 11:27:09 -0400

    Update college_data.py

--- chance_me/college_data.py
+++ chance_me/college_data.py
@@ -37,7 +37,21 @@ COLLEGES = {
             "Business": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
             "Pre-Med/Health": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
             "Undecided": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2}
-        }
+        
+        "academic_weight": 0.45,
+        "activity_weight": 0.35,
+        "misc_weight": 0.20,
+            
+        "sat_math_weight": 0.45,
+        "sat_rw_weight": 0.55,
+        
+        "preferences": {
+            "leadership": 1.25,
+            "research": 1.10,
+            "volunteering": 1.15,
+            "sports": 1.10,
+            "arts": 1.10
+                }
     },
     "Stanford University": {
         "avg_sat": 1500,
@@ -80,6 +94,22 @@ COLLEGES = {
             "Pre-Med/Health": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
             "Undecided": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2}
         }
+        
+        "academic_weight": 0.47,
+        "activity_weight": 0.33,
+        "misc_weight": 0.20,
+        
+        "sat_math_weight": 0.48,
+        "sat_rw_weight": 0.52,
+        
+        "preferences": {
+            "leadership": 1.15,
+            "research": 1.20,
+            "arts": 1.20,
+            "volunteering": 1.15,
+            "music": 1.15
+        }
+    
     },
     "Princeton University": {
         "avg_sat": 1500,
@@ -93,6 +123,20 @@ COLLEGES = {
             "Business": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
             "Pre-Med/Health": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
             "Undecided": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2}
+        }
+        "academic_weight": 0.52,
+        "activity_weight": 0.28,
+        "misc_weight": 0.20,
+        
+        "sat_math_weight": 0.52,
+        "sat_rw_weight": 0.48,
+        
+        "preferences": {
+            "research": 1.25,
+            "academic": 1.20,
+            "leadership": 1.10,
+            "volunteering": 1.05,
+            "sports": 1.05
         }
     },
     "University of California, Berkeley": {
@@ -136,5 +180,149 @@ COLLEGES = {
             "Pre-Med/Health": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
             "Undecided": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2}
         }
+    },
+        
+     "Columbia University": {
+        "avg_sat": 1510,
+        "avg_gpa": 3.95,
+        "selectivity": 0.04,
+
+        "major_weights": {
+            "STEM": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
+            "Humanities": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2},
+            "Social Sciences": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2},
+            "Arts": {"academic_weight": 0.4, "activity_weight": 0.4, "misc_weight": 0.2},
+            "Business": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
+            "Pre-Med/Health": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
+            "Undecided": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2}
+        },
+
+        "sat_math_weight": 0.50,
+        "sat_rw_weight": 0.50,
+
+        "preferences": {
+            "research": 1.15,
+            "leadership": 1.20,
+            "internships": 1.25,
+            "volunteering": 1.10,
+            "arts": 1.10
+        }
+    },
+
+    "University of Pennsylvania": {
+        "avg_sat": 1500,
+        "avg_gpa": 3.93,
+        "selectivity": 0.05,
+
+        "major_weights": {
+            "STEM": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
+            "Humanities": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2},
+            "Social Sciences": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2},
+            "Arts": {"academic_weight": 0.4, "activity_weight": 0.4, "misc_weight": 0.2},
+            "Business": {"academic_weight": 0.6, "activity_weight": 0.3, "misc_weight": 0.1},
+            "Pre-Med/Health": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
+            "Undecided": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2}
+        },
+
+        "sat_math_weight": 0.53,
+        "sat_rw_weight": 0.47,
+
+        "preferences": {
+            "leadership": 1.30,
+            "internships": 1.30,
+            "entrepreneurship": 1.25,
+            "business": 1.25,
+            "research": 1.05
+        }
+    },
+
+    "Brown University": {
+        "avg_sat": 1500,
+        "avg_gpa": 3.95,
+        "selectivity": 0.05,
+
+        "major_weights": {
+            "STEM": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
+            "Humanities": {"academic_weight": 0.4, "activity_weight": 0.4, "misc_weight": 0.2},
+            "Social Sciences": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2},
+            "Arts": {"academic_weight": 0.35, "activity_weight": 0.45, "misc_weight": 0.2},
+            "Business": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2},
+            "Pre-Med/Health": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
+            "Undecided": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2}
+        },
+
+        "sat_math_weight": 0.47,
+        "sat_rw_weight": 0.53,
+
+        "preferences": {
+            "arts": 1.20,
+            "volunteering": 1.20,
+            "leadership": 1.10,
+            "research": 1.10,
+            "nontraditional": 1.25
+        }
+    },
+
+    "Cornell University": {
+        "avg_sat": 1480,
+        "avg_gpa": 3.9,
+        "selectivity": 0.07,
+
+        "major_weights": {
+            "STEM": {"academic_weight": 0.65, "activity_weight": 0.2, "misc_weight": 0.15},
+            "Humanities": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2},
+            "Social Sciences": {"academic_weight": 0.55, "activity_weight": 0.25, "misc_weight": 0.2},
+            "Arts": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2},
+            "Business": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
+            "Pre-Med/Health": {"academic_weight": 0.6, "activity_weight": 0.25, "misc_weight": 0.15},
+            "Undecided": {"academic_weight": 0.55, "activity_weight": 0.25, "misc_weight": 0.2}
+        },
+
+        "sat_math_weight": 0.55,
+        "sat_rw_weight": 0.45,
+
+        "preferences": {
+            "research": 1.20,
+            "academic": 1.20,
+            "engineering": 1.25,
+            "leadership": 1.10,
+            "volunteering": 1.05,
+            "arts": 1.00
+        }
+    },
+
+    "Dartmouth College": {
+        "avg_sat": 1500,
+        "avg_gpa": 3.9,
+        "selectivity": 0.06,
+
+        "major_weights": {
+            "STEM": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
+            "Humanities": {"academic_weight": 0.4, "activity_weight": 0.4, "misc_weight": 0.2},
+            "Social Sciences": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2},
+            "Arts": {"academic_weight": 0.35, "activity_weight": 0.45, "misc_weight": 0.2},
+            "Business": {"academic_weight": 0.5, "activity_weight": 0.3, "misc_weight": 0.2},
+            "Pre-Med/Health": {"academic_weight": 0.55, "activity_weight": 0.3, "misc_weight": 0.15},
+            "Undecided": {"academic_weight": 0.45, "activity_weight": 0.35, "misc_weight": 0.2}
+        },
+
+        "sat_math_weight": 0.50,
+        "sat_rw_weight": 0.50,
+
+        "preferences": {
+            "leadership": 1.25,
+            "volunteering": 1.20,
+            "sports": 1.15,
+            "outdoors": 1.30,
+            "community": 1.25,
+            "research": 1.10
+        }
     }
+
 }
+
+    
+    
+    
+    
+    }
